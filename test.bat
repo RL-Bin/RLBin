74c6595cf49068c1d4874051f421713b456f5c90
amirmajlesikupaei <amir.majlesi.kupaei@gmail.com>
2019-09-10 10:35:13 -0400

    adding test executables to repo

--- test.bat
+++ test.bat
@@ -1,5 +1,7 @@
 rmdir .\analysis /s /q
 
+RLBin tests\lbm_r\lbm_r.exe
+
 rem RLBin tests\lbm_r\lbm_r.exe 1 reference.dat 0 1 tests\lbm_r\100_100_130_cf_a.of
 
-RLBin tests\lbm_r\lbm_r.exe 20 reference.dat 0 1 tests\lbm_r\100_100_130_cf_a.of
+rem RLBin tests\lbm_r\lbm_r.exe 20 reference.dat 0 1 tests\lbm_r\100_100_130_cf_a.of

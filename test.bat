b7b5b37fb50c6b8417cc57216b5e416854b109c0
amirmajlesikupaei <amir.majlesi.kupaei@gmail.com>
2019-09-13 12:00:19 -0400

    minor changes

--- test.bat
+++ test.bat
@@ -1,7 +1,3 @@
 rmdir .\analysis /s /q
 
-RLBin tests\lbm_r\lbm_r.exe
-
-rem RLBin tests\lbm_r\lbm_r.exe 1 reference.dat 0 1 tests\lbm_r\100_100_130_cf_a.of
-
-rem RLBin tests\lbm_r\lbm_r.exe 20 reference.dat 0 1 tests\lbm_r\100_100_130_cf_a.of
+RLBin --dynamic --verbose -o1 tests\lbm_r\lbm_r.exe 20 reference.dat 0 1 tests\lbm_r\100_100_130_cf_a.of
